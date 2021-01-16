3fe3f185e1ba1cc7cae62c1d12beaa310d35dce9
Dsky <dutian1990@gmail.com>
2021-01-16 23:16:36 +0800

    Add files via upload

---
+++ precache-manifest.a35fd994f3017baad632cf1c0c40f70a.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5c0f7218fd55b046b83c50bc1363cd5",
+    "url": "https://cdn.jsdelivr.net/gh/chickenswap/dollar@main/index.html"
+  },
+  {
+    "revision": "675c2e03d7b6d4ab28dc",
+    "url": "https://cdn.jsdelivr.net/gh/chickenswap/dollar@main/static/css/main.c44efd69.chunk.css"
+  },
+  {
+    "revision": "2d567dc50b609301bad5",
+    "url": "https://cdn.jsdelivr.net/gh/chickenswap/dollar@main/static/js/0.aee7f6b8.chunk.js"
+  },
+  {
+    "revision": "7dd016441e3f33af1bd6",
+    "url": "https://cdn.jsdelivr.net/gh/chickenswap/dollar@main/static/js/10.4756a570.chunk.js"
+  },
+  {
+    "revision": "887b8d35f70be4544bba",
+    "url": "https://cdn.jsdelivr.net/gh/chickenswap/dollar@main/static/js/11.e70a8b98.chunk.js"
+  },
+  {
+    "revision": "70165dd8ec880a0a4584",
+    "url": "https://cdn.jsdelivr.net/gh/chickenswap/dollar@main/static/js/3.96f98636.chunk.js"
+  },
+  {
+    "revision": "2e3e6d043e668914cdb920cd9b235c12",
+    "url": "https://cdn.jsdelivr.net/gh/chickenswap/dollar@main/static/js/3.96f98636.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25216e5952ae1e114f92",
+    "url": "https://cdn.jsdelivr.net/gh/chickenswap/dollar@main/static/js/4.0a17e0fb.chunk.js"
+  },
+  {
+    "revision": "a91779b33224815e53e5",
+    "url": "https://cdn.jsdelivr.net/gh/chickenswap/dollar@main/static/js/5.ef45f15a.chunk.js"
+  },
+  {
+    "revision": "374658371e5d9c95e00ea15067d24dad",
+    "url": "https://cdn.jsdelivr.net/gh/chickenswap/dollar@main/static/js/5.ef45f15a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f508077875f855e61261",
+    "url": "https://cdn.jsdelivr.net/gh/chickenswap/dollar@main/static/js/6.6693b007.chunk.js"
+  },
+  {
+    "revision": "88e5053053539c8b4a53480943cb0e08",
+    "url": "https://cdn.jsdelivr.net/gh/chickenswap/dollar@main/static/js/6.6693b007.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17fbbecac2b60c39b049",
+    "url": "https://cdn.jsdelivr.net/gh/chickenswap/dollar@main/static/js/7.1374f84d.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "https://cdn.jsdelivr.net/gh/chickenswap/dollar@main/static/js/7.1374f84d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5fb5d42f81579fdad0c",
+    "url": "https://cdn.jsdelivr.net/gh/chickenswap/dollar@main/static/js/8.c5e9586c.chunk.js"
+  },
+  {
+    "revision": "d43bcb65c23262ca7bd991f5625a0c00",
+    "url": "https://cdn.jsdelivr.net/gh/chickenswap/dollar@main/static/js/8.c5e9586c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d86b90bb690e5ad9846c",
+    "url": "https://cdn.jsdelivr.net/gh/chickenswap/dollar@main/static/js/9.e4f0bd4e.chunk.js"
+  },
+  {
+    "revision": "675c2e03d7b6d4ab28dc",
+    "url": "https://cdn.jsdelivr.net/gh/chickenswap/dollar@main/static/js/main.d6233581.chunk.js"
+  },
+  {
+    "revision": "488edb3e5d7a06642c4a",
+    "url": "https://cdn.jsdelivr.net/gh/chickenswap/dollar@main/static/js/runtime-main.30acb758.js"
+  },
+  {
+    "revision": "e5e6c1aaacdd6358ed15a79e7df6e16d",
+    "url": "https://cdn.jsdelivr.net/gh/chickenswap/dollar@main/static/media/Bank.e5e6c1aa.png"
+  },
+  {
+    "revision": "839a0f0d6e0a69fed8d7846344d2100b",
+    "url": "https://cdn.jsdelivr.net/gh/chickenswap/dollar@main/static/media/USDT.839a0f0d.png"
+  },
+  {
+    "revision": "8632863f33cbd416d626e657d988ac27",
+    "url": "https://cdn.jsdelivr.net/gh/chickenswap/dollar@main/static/media/Welcome.8632863f.png"
+  },
+  {
+    "revision": "89e2967875c805010eda6e2a11917411",
+    "url": "https://cdn.jsdelivr.net/gh/chickenswap/dollar@main/static/media/bac.89e29678.svg"
+  },
+  {
+    "revision": "9c110528bfb443efc9a0952842e16103",
+    "url": "https://cdn.jsdelivr.net/gh/chickenswap/dollar@main/static/media/farmer.9c110528.png"
+  },
+  {
+    "revision": "559ee3ae662b64147a603e6ceb15d487",
+    "url": "https://cdn.jsdelivr.net/gh/chickenswap/dollar@main/static/media/join.559ee3ae.png"
+  },
+  {
+    "revision": "f0ae9a54b6ea8b043624844526d7850e",
+    "url": "https://cdn.jsdelivr.net/gh/chickenswap/dollar@main/static/media/kfc-bond-logo.f0ae9a54.png"
+  },
+  {
+    "revision": "bf8f793fd0fdc6d3a20800177832a9d1",
+    "url": "https://cdn.jsdelivr.net/gh/chickenswap/dollar@main/static/media/kfc-cash-logo.bf8f793f.png"
+  },
+  {
+    "revision": "1fe975fd87e84a9047a7e013838ebee7",
+    "url": "https://cdn.jsdelivr.net/gh/chickenswap/dollar@main/static/media/kfc-share-logo.1fe975fd.png"
+  }
+]);
